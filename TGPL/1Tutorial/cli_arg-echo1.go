a6112ca8f6d52862a86fe971a3e25f4954742f1c
hobbiathan <hobbiathan@gmail.com>
2022-06-30 16:51:54 -0600

    formatting

--- TGPL/1Tutorial/cli_arg-echo1.go
+++ TGPL/1Tutorial/cli_arg-echo1.go
@@ -6,12 +6,12 @@ import (
 ) 
 
 func main() {
-	var s, sep string // Declaring string variables 's', 'sep'
+	var s, sep string 			// Declaring string variables 's', 'sep'
 
-	for i := 1; i < len(os.Args); i++ { // Loop through all CLI arguments minus the initial
+	for i := 1; i < len(os.Args); i++ { 	// Loop through all CLI arguments minus the initial
 
-		s += sep + os.Args[i] // Store results in 's'
-		sep = " " // Spacing after the first argument is stored
+		s += sep + os.Args[i] 		// Store results in 's'
+		sep = " " 			// Spacing after the first argument is stored
 	}
 
 		fmt.Println(s) 
